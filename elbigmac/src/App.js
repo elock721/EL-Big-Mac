9aeaacdbb2ab50b0b3c7488641fee5238acdc8ef
Elliott Lockwood <lockwood721@gmail.com>
2020-09-29 14:28:58 -0500

    input group addition

--- elbigmac/src/App.js
+++ elbigmac/src/App.js
@@ -2,9 +2,9 @@ import React, { useState } from "react";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Jumbotron from "react-bootstrap/Jumbotron";
-import Toast from "react-bootstrap/Toast";
 import Container from "react-bootstrap/Container";
-
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/FormControl";
 
 
 import "./App.css";
@@ -14,9 +14,19 @@ import "./App.css";
 const App = () => (
   <Container className="p-3">
     <Jumbotron>
-      <h1 className="header">Welcome To React-Bootstrap</h1>
-      
+      <h1 className="header">McCurrency</h1>
+      <h3>How many Big Mac's can you get?</h3>
     </Jumbotron>
+    <h1>Please enter an amount of money in your local currency 😃 </h1>
+    <InputGroup size="lg">
+    <InputGroup.Prepend>
+      <InputGroup.Text id="inputGroup-sizing-lg">$</InputGroup.Text>
+    </InputGroup.Prepend>
+    <FormControl aria-label="Large" aria-describedby="inputGroup-sizing-sm" />
+  </InputGroup>
+  
+
+
   </Container>
 );
 
